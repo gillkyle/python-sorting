e2d938b4aef64ec29f273f1d607e2f496b65d0b9
gillkyle <kylerobertgill@gmail.com>
2019-02-20 10:18:45 -0700

    main setup

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from sorters import bubble_sort, insertion_sort, selection_sort
 from collections import namedtuple
-
+import re
 
 # data for a single word
 WordData = namedtuple("WordData", ['word', 'count', 'percent'])
+
+WORD_SIZE = 5
 
 
 def get_data():
@@ -14,20 +16,27 @@ def get_data():
 
     # Convert the entire string to lowercase.
     bom = bom.lower()
-    print(bom)
 
     # Split the string by any non-alpha character. Regular expressions are your friend here.
     # A simple regular expression with `re.split(...)` will do this for you.
+    words = re.split(r"[^A-Za-z]", bom)
 
     # Using a list comprehension with a conditional (if), create a new list that contains only
     # those words that are 5+ alpha characters in length. All of the following will be
     # skipped: "am", "", "i", "are".
+    filtered_words = [x for x in words if len(x) >= WORD_SIZE]
 
     # Count the frequency of each word in the list, creating a WordData object for each
     # unique word.  Round all percentages to three decimal places: 3.141592 => 3.142.
     # See the `collections.Counter` module is your friend here.  The percent for a given
     # word is calculated as `count / length of list`, rounded to one decimal place.
     data = []
+    counts = dict()
+    for i in filtered_words:
+        counts[i] = counts.get(i, 0) + 1
+    total_words = len(filtered_words)
+    for word, count in counts.items():
+        data.append(WordData(word, count, round((count/total_words)*100, 3)))
 
     # return the list of WordData objects, which contains
     # a single object for each unique word
